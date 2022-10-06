5c4bc13dba9a8490e851a6f2c988b8e60689ee06
unknown <rocky55737@gmail.com>
2022-10-07 01:38:03 +0900

    1.2

--- src/so_long.c
+++ src/so_long.c
@@ -15,7 +15,16 @@
 void	so_long(int ac, char **av)
 {
 	t_map	*map;
+	void	*mlx;
+	void	*mlx_window;
 
 	sl_input_err_handler(ac);
 	map = get_map(av[1]);
+	mlx = mlx_init();
+	mlx_window = mlx_new_window(mlx, map->x * , map->y * , "so_long");
+
+	put_map_to_window(map);
+
+	mlx_hook(mlx_window, )
+	mlx_loop(mlx);
 }
