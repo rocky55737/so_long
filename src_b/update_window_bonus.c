f7f74f45bfbb7f9832a3c88e798d197e79db070e
unknown <rocky55737@gmail.com>
2022-10-19 09:05:26 +0900

    update window bonus leaks

--- src_b/update_window_bonus.c
+++ src_b/update_window_bonus.c
@@ -19,9 +19,13 @@ void	move_character(int keycode, t_game_data *g_data);
 
 int	update_window(int keycode, t_game_data *g_data)
 {
+	char	*buffer;
+
 	move_character(keycode, g_data);
 	put_map_to_window(g_data);
 	g_data->mov_cnt++;
-	mlx_string_put(g_data->mlx, g_data->win, 3, 3, 0, ft_itoa(g_data->mov_cnt));
+	buffer = ft_itoa(g_data->mov_cnt);
+	mlx_string_put(g_data->mlx, g_data->win, 3, 3, 0, buffer);
+	free(buffer);
 	return (0);
 }
