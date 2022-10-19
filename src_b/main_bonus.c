f13da56b9c82e31488e931aa2f08f5ba5ac28962
RHONG <rocky55737@gmail.com>
2022-10-19 16:50:51 +0900

    complete

--- src_b/main_bonus.c
+++ src_b/main_bonus.c
@@ -6,20 +6,14 @@
 /*   By: rhong <rhong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:24:05 by rhong             #+#    #+#             */
-/*   Updated: 2022/10/18 20:04:05 by rhong            ###   ########.fr       */
+/*   Updated: 2022/10/19 16:44:21 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-void leaks(void)
-{
-	system("leaks so_long");
-}
-
 int	main(int ac, char **av)
 {
-	atexit(leaks);
 	so_long(ac, av);
 	return (0);
 }
